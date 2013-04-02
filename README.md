60d9711a11a09eef4722007541f8206be6b3c15c
David Carboni <david.carboni@workdocx.com>
2013-04-02 16:52:02 +0100

    Initial import.

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ thread-dumper
 =============
 
 Super-simple thread-dump utility for watching JVM performance as you develop.
+
+This is great if you're developing a Java webapp and want to see what's taking time. Thread-dumper
+takes a thread dump every second and splits it by blank lines to identify individual thread stacks. 
+This isn't revolutionary, but is remarkably useful. When something in your webapp takes a while to 
+respond, a few clicks gives you a remarkably clear view of what's slowing you down. 
+Simple, effective.
+
+Requirements:
+ You'll need "jstack" available on your path. This is a JVM tool.
+
+Build:
+ mvn assembly:assembly
+
+Run:
+ java -jar thread-dumper-1.0.0-jar-with-dependencies.jar <pid> [search terms]
+
+Search terms:
+ If you provide no search terms, thread-dumper will print out a full thread dump every second.
+ Thread-dumper will show any "blocks of text" - typically thread stacks - that contain the search terms you specify.
+ If you need to suppress something, use -<search term>.
+ 
+Examples:
+  java -jar thread-dumper-1.0.0-jar-with-dependencies.jar 6884 +com.mypackage -com.mypackage.BackgroundProcessor
+   is equivalent to:
+  java -jar thread-dumper-1.0.0-jar-with-dependencies.jar 6884 com.mypackage -com.mypackage.BackgroundProcessor
+
+I hope it's as useful for you as it is for me.
+
+David
---
+++ src/main/java/org/workdocx/threaddump/Main.java
@@ -0,0 +1,156 @@
+package org.workdocx.threaddump;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
+
+public class Main {
+
+	public static final String COMMAND = "jstack";
+	public static final long INTERVAL = 1000;
+
+	private static int pid;
+	private static List<String> searches = new ArrayList<>();
+	private static List<String> excludes = new ArrayList<>();
+	private static InputStream inputStream;
+
+	public static void main(String[] args) throws IOException {
+		readParameters(args);
+		dump();
+	}
+
+	private static void dump() throws IOException {
+
+		do {
+			ProcessBuilder processBuilder = new ProcessBuilder(COMMAND, String.valueOf(pid));
+			Process process = processBuilder.start();
+			inputStream = new BufferedInputStream(process.getInputStream());
+
+			// Read the output:
+			List<String> readLines;
+			try {
+				readLines = IOUtils.readLines(inputStream);
+			} finally {
+				IOUtils.closeQuietly(inputStream);
+			}
+
+			// Select output:
+			if (searches.size() == 0) {
+				// Print everything:
+				for (String line : readLines) {
+					System.out.println(line);
+				}
+			} else {
+
+				List<List<String>> threads = splitThreads(readLines);
+				for (List<String> thread : threads) {
+
+					// Does this thread contain any frames we're interested in?
+					boolean exclude = false;
+					boolean print = false;
+					for (String line : thread) {
+						for (String search : searches) {
+							if (StringUtils.containsIgnoreCase(line, search)) {
+								print = true;
+							}
+						}
+						for (String search : excludes) {
+							if (StringUtils.containsIgnoreCase(line, search)) {
+								exclude = true;
+							}
+						}
+					}
+
+					// Print away, Jeeves:
+					if (print && !exclude) {
+						for (String line : thread) {
+							System.out.println(line);
+						}
+						System.out.println();
+					}
+				}
+			}
+
+			// Take a breath:
+			sleep();
+
+//			System.out.println();
+//			System.out.println("---");
+//			System.out.println();
+
+		} while (true);
+	}
+
+	private static List<List<String>> splitThreads(List<String> readLines) {
+		List<List<String>> result = new ArrayList<>();
+		List<String> thread = new ArrayList<>();
+		result.add(thread);
+
+		int i = 0;
+		while (i < readLines.size()) {
+
+			while (i < readLines.size() && StringUtils.isNotBlank(readLines.get(i))) {
+//				System.out.println("adding: " + readLines.get(i));
+				thread.add(readLines.get(i));
+				i++;
+			}
+
+			// Create a new list for the next thread:
+			if (i < readLines.size() && result.get(result.size() - 1).size() != 0) {
+//				System.out.println("Next thread..");
+				thread = new ArrayList<>();
+				result.add(thread);
+				i++;
+			}
+		}
+
+		return result;
+	}
+
+	private static void sleep() {
+		try {
+			Thread.sleep(INTERVAL);
+		} catch (InterruptedException e) {
+			System.out.println(ExceptionUtils.getStackTrace(e));
+		}
+	}
+
+	/**
+	 * Reads the process ID parameter.
+	 * 
+	 * @param args
+	 *            The command-line parameters.
+	 */
+	private static void readParameters(String[] args) {
+
+		// Get the PID:
+		try {
+			pid = Integer.parseInt(args[0]);
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Please provide a Java process ID.");
+			System.exit(1);
+		} catch (NumberFormatException e) {
+			System.out.println("Unable to parse Java process ID: [" + args[0] + "]");
+			System.exit(1);
+		}
+
+		// Get the search/exclude strings:
+		int i = 1;
+		while (i < args.length) {
+			String search = args[i++];
+			if (search.startsWith("-")) {
+				excludes.add(search.substring(1));
+			} else if (search.startsWith("+")) {
+				searches.add(search.substring(1));
+			} else {
+				searches.add(search);
+			}
+		}
+	}
+}
